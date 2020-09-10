421835a3955639977a7f99cf4f740b812f06526a
Acid Bubbles <acidbubbles.anon@gmail.com>
2020-09-09 21:59:02 -0400

    Very simple, hardcoded proof of concept

--- src/Jiggle.cs
+++ src/Jiggle.cs
@@ -1,12 +1,15 @@
 using System;
+using UnityEngine;
 
 public class Jiggle : MVRScript
 {
+    private Transform _cua;
+    private DynamicBone _dynamicBone;
+
     public override void Init()
     {
         try
         {
-            SuperController.LogMessage($"{nameof(Jiggle)} initialized");
         }
         catch (Exception e)
         {
@@ -18,7 +21,13 @@ public class Jiggle : MVRScript
     {
         try
         {
-            SuperController.LogMessage($"{nameof(Jiggle)} enabled");
+            var containingAtom = GetAtom();
+            var loader = containingAtom.GetComponentInChildren<CustomUnityAssetLoader>();
+            if (loader == null) throw new InvalidOperationException($"Atom does not have a {nameof(CustomUnityAssetLoader)}");
+            if (loader.transform.childCount == 0) throw new InvalidOperationException($"No unity asset has been loaded");
+            _cua = loader.transform.GetChild(0);
+            _dynamicBone = _cua.gameObject.AddComponent<DynamicBone>();
+            _dynamicBone.m_Root = _cua.GetChild(1);
         }
         catch (Exception e)
         {
@@ -30,7 +39,6 @@ public class Jiggle : MVRScript
     {
         try
         {
-            SuperController.LogMessage($"{nameof(Jiggle)} disabled");
         }
         catch (Exception e)
         {
@@ -42,11 +50,22 @@ public class Jiggle : MVRScript
     {
         try
         {
-            SuperController.LogMessage($"{nameof(Jiggle)} destroyed");
+            if (_dynamicBone != null) Destroy(_dynamicBone);
         }
         catch (Exception e)
         {
             SuperController.LogError($"{nameof(Jiggle)}.{nameof(OnDestroy)}: {e}");
         }
     }
+
+        private Atom GetAtom()
+        {
+            // Note: Yeah, that's horrible, but containingAtom is null
+            var container = gameObject?.transform?.parent?.parent?.parent?.parent?.parent?.gameObject;
+            if (container == null) throw new NullReferenceException($"Could not find the parent gameObject while looking for the containing atom.");
+            var atom = container.GetComponent<Atom>();
+            if (atom == null) throw new NullReferenceException($"Could not find the parent atom in {container.name}.");
+            if (atom.type != "CustomUnityAsset") throw new InvalidOperationException("Can only be applied on CustomUnityAsset.");
+            return atom;
+        }
 }
